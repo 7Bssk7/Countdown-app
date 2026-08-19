d29549b3e0174b9f56e0eb0e4c5299ca0c2876b2
7Bssk7 <arhip.fin3@gmail.com>
2026-08-18 19:25:48 -0700

    Third commit

--- countdown.py
+++ countdown.py
@@ -1,5 +1,32 @@
-import tkinter as tk 
+import time
+from tkinter import *
+from tkinter import messagebox
+from datetime import date
 
-root = tk.Tk()
+def calculate_day():
+    target_date = date(2026, 9, 28)
+    today = date.today
+
+    days_left = (target_date - today).days
+
+    if days_left > 0:
+        num_days.set(f"{days_left}")
+
+root = Tk()
+
+root.geometry("320x250")
+root.title("My Countdown")
+
+num_days = StringVar()
+
+
+calculate_day()
+
+
+
+numDaysEntry = Entry(root, width = 3, font=("Arial", 18, ""), textvariable=num_days)
+
+numDaysEntry.place(x= 80, y= 20)
+
 
 root.mainloop()
